de4e0e431dd016be183629b1683a115bc9e4e2d8
Jae <jcho7@pride.hofstra.edu>
2021-11-29 03:47:38 -0500

    Got most of thing to work except adding items to shopping cart

--- src/modules/my/header/header.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class App extends LightningElement {
-
-	@api
-	compName = "keeb.";
-}
--- src/modules/my/product/product.js
+++ src/modules/my/product/product.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+	@track
+	itemList = ["Test1", "Test2"];
+
+	@track
+	items = [
+		{name: "65%Keeb"}
+	]
+
+	addItem() {
+		let images = this.template.querySelector("img");
+		let imgName = images.value;
+
+		for(let image of images) {
+			image.addEventListener("click", this.itemList.push(imgName));
+		}
+	}
+}
